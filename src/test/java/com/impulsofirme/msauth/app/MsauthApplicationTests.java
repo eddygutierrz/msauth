8ee2cf3375e567901967c3f6a6430c740eeb0189
eddy.gutierrez <eddy.gutierrez@remtools.mx>
2025-08-08 23:07:08 -0600

    Primer commit - estructura inicial

---
+++ src/test/java/com/impulsofirme/msauth/app/MsauthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.impulsofirme.msauth.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
