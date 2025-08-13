d17451b033651d6c9d35d87915e7d27eacd4d7f3
eddy.gutierrez <eddy.gutierrez@remtools.mx>
2025-08-12 22:46:55 -0600

    Ajustes para seguridad en msauth

--- src/main/java/com/impulsofirme/msauth/app/config/SecurityConfig.java
+++ src/main/java/com/impulsofirme/msauth/app/config/SecurityConfig.java
@@ -18,15 +18,19 @@ public class SecurityConfig {
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-            .csrf(csrf -> csrf.disable())
-            .cors(c -> {})
-            .authorizeHttpRequests(auth -> auth
-                .requestMatchers(
-                    "/api/auth/**", 
-                    "/auth/validate"
-                    ).permitAll()
-                .anyRequest().authenticated()
-            );
+      // Desactiva CSRF (o ignora el path de auth)
+      .csrf(csrf -> csrf.disable())
+      // .csrf(csrf -> csrf.ignoringRequestMatchers("/api/auth/**"))
+
+      // CORS
+      .cors(c -> c.configurationSource(corsConfigurationSource()))
+
+      // Permisos
+      .authorizeHttpRequests(auth -> auth
+        .requestMatchers(org.springframework.http.HttpMethod.OPTIONS, "/**").permitAll()
+        .requestMatchers("/api/auth/**").permitAll()
+        .anyRequest().authenticated()
+      );
         return http.build();
     }
 
